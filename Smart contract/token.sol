2d7806e6a41c124533f8e81a55149284b1c0a70e
SACHIN KUMAR YADAV <116642788+SACHINKUMAR1728@users.noreply.github.com>
2023-05-20 14:10:13 +0530

    Update token.sol

--- Smart contract/token.sol
+++ Smart contract/token.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract HUSTLETOKEN is ERC20 ("HUSTLE TOKEN","HSTL"),Ownable {
+
+function mintFifty() public  onlyOwner {
+    _mint(msg.sender, 809843 * 10**18);
+}
+    
+}
